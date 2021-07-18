9ebc056765e947e8663e9dc1a758df95986040c8
Alexander Fatyuhin <virikidorhom@gmail.com>
2021-07-18 16:41:23 +0300

    статистика (первый вариант для базы банных)

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ParseStat;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +12,19 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        $parseStats = false;
+
+        $parseStats = ParseStat::all()->sortDesc()->toArray();
+
+        foreach ($parseStats as $k => $item) {
+            $parseStats[$k]['created_at'] = Carbon::parse($item['created_at'])->format('Y-m-d H-m-i');
+        }
+
+        return view('index', [
+            'parseStats' => $parseStats
+        ]);
+    }
 }
