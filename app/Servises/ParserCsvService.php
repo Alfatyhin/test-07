40f6132b56ba4300eed1710b4400379dd87e9f8d
Alexander Fatyuhin <virikidorhom@gmail.com>
2021-07-18 21:02:25 +0300

    сервис для парсинга csv родительский класс

---
+++ app/Servises/ParserCsvService.php
@@ -0,0 +1,80 @@
+<?php
+
+
+namespace App\Servises;
+
+
+class ParserCsvService
+{
+
+    private $headers;
+    private $collectionLines = [];
+
+    public function __construct()
+    {
+
+    }
+
+    public function addCollectionLies(array $data) : self // типизация - то чо должна возвращать функция??
+    {
+        if (!$this->headers) {
+            $this->headers = $data;
+        } else {
+            $collection = $this->collectionLines;
+            $collection[] = $data;
+            $this->collectionLines = $collection;
+
+        }
+
+        return $this;
+    }
+
+    public function toArray() : array
+    {
+        foreach ($this->collectionLines as $item) {
+            $itemData[] = array_combine($this->headers, $item);
+        }
+
+        return $itemData;
+    }
+
+    public static function parseCsvString(string $str) : array
+    {
+        $data = str_getcsv($str);
+
+        return $data;
+    }
+
+    public static function getSubArrayUnicue(array $data) : array
+    {
+        $valueData = array_unique($data);
+
+        foreach ($valueData as $str) {
+
+            if (empty($str)) {
+                $str = 'None';
+            }
+
+            $strArray = self::parseCsvString($str);
+            foreach ($strArray as $valName) {
+                $valName = trim($valName);
+                $subData[] = $valName;
+            }
+        }
+        $subData = array_unique($subData);
+
+        return $subData;
+    }
+
+    public function getCollectionLines() : array
+    {
+        return $this->collectionLines;
+    }
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+
+
+}
